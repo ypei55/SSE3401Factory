ad9820bd0d111854a47ea76d2b77e1253f4be584
ypei55 <ypei0505@gmail.com>
2024-05-14 23:13:09 +0800

    update

--- android/app/src/androidTest/java/com/example/lab5_factory/MainActivityTest.java
+++ android/app/src/androidTest/java/com/example/lab5_factory/MainActivityTest.java
@@ -1,7 +1,7 @@
 package com.example.lab5_factory;
 
 import androidx.test.rule.ActivityTestRule;
-import dev.flutter.plugins.integration_test.FlutterRunner;
+import dev.flutter.plugins.integration_test.FlutterTestRunner;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
 
